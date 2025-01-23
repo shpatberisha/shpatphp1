34a6f3be8f1a3a05b7faca8b1da4cde5d4caea7d
shpatberisha <shpatberisha2510@gmail.com>
2025-01-23 16:16:34 +0100

    php1

---
+++ moduli2/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$klubi="Barcelona";
+$mosha="15";
+$pesha="4.5";
+
+
+echo $klubi;
+echo $mosha;
+echo $pesha;
+
+$x=134;
+$y=123;
+
+echo $x + $y;
+
+
+$fjalia = "I love php";
+echo str_replace("love","dont love", $fjalia);
+echo strlen($fjalia);
+?>
