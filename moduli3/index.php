d22c87727f2c5cc194af43a7198d2c8038db6c25
shpatberisha <shpatberisha2510@gmail.com>
2025-01-28 16:17:44 +0100

    shpati

---
+++ moduli3/index.php
@@ -0,0 +1,57 @@
+<?php
+
+$notat = 80;
+ 
+if($notat >= 90){
+    echo "YOU GOT 5"
+}
+
+if($notat<= 89 && $notat >80 ){
+    echo "YOU GOT 4"
+}
+
+if($notat <= 70 && $notat >79){
+    echo "YOU GOT 3"
+}
+
+if($notat<= 60 && $notat >69){
+    echo "YOU GOT 2"
+}
+
+if($notat<= 50 ){
+    echo "YOU GOT "
+
+
+
+ $day "tuesday"
+
+
+ switch ($day) {
+    case "monday":
+        echo "nuk ki kurs";
+        break;
+
+                    case "tuesday":
+                        echo "ki kurs";
+                        break;
+
+                            case "wendesday":
+                                echo "nuk ki kurs";
+                                break;
+
+                                    case "thursday":
+                                        echo " ki kurs";
+                                        break;
+
+                              
+                                            case "friday":
+                                                echo "nuk ki kurs";
+                                                break;
+
+                                                default:
+                                                echo "vikend";
+                                                break;
+
+ }
+
+?>
