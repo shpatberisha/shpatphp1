33d4b353b09c9de20211c2451761e634ae94cf92
shpatberisha <shpatberisha2510@gmail.com>
2025-02-13 16:18:46 +0100

    phpp

---
+++ module5/easy.php
@@ -0,0 +1,35 @@
+<?php
+
+$punotoret = array(
+
+    array(
+        "name" => "Shpat";
+        "age"  => "15"; 
+        "position" => "students"      
+    ),
+    array(
+        "name" => "Bledi";
+        "age"  => "16"; 
+        "position" => "students"      
+    ),
+    array(
+        "name" => "Andi";
+        "age"  => "15"; 
+        "position" => "students"      
+    ),
+    
+    
+
+
+)
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ module5/medium.php
@@ -0,0 +1,27 @@
+<?php
+
+$products=array(
+    array("name" => "milka"; 
+    "age" => 5 ),
+    arry("name" => "rugova";
+    "price" => 0.5)
+)
+
+function price($products){
+  $sum=0;
+foreach($products as $products){
+
+    $sum + = $products["price"]
+    
+}
+return $sum;
+
+price($products)
+
+
+}
+
+
+
+
+?>
